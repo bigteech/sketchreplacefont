67750c905004083e97d3f54ef0ce3353ed636470
wangweipeng <weipeng.wang@gllue.com>
2018-11-10 00:06:04 +0800

    update

--- src/main.js
+++ src/main.js
@@ -4,50 +4,52 @@ var UI = require('sketch/ui')
 var dom = require('sketch/dom')
 
 
+function changePage(one, from, to) {
+				if(from.indexOf(to.id) > -1) {
+								one.sharedStyleId = to.id;
+				}
+				one.affectedLayer && changePage(one.affectedLayer, from, to)
+				one.overrides && one.overrides.forEach(y => changePage(y, from, to))
+				one.layers && one.layers.forEach(y => changePage(y, from, to))
+
+}
+
+
 
 export default function(context) {
-  const document = sketch.fromNative(context.document)
-  const selection = document.selectedLayers
-  let dialog = ui.dialog('合并', '');
-  let checkboxs = []
-  selection.forEach(x => {
-    let checkbox = ui.checkbox(false, x.name);
-    checkboxs.push(checkbox)
-    dialog.addAccessoryView(checkbox);
-  })
+				try{
 
-  let responseCode = dialog.runModal();
+								const document = sketch.fromNative(context.document)
+								let dialog = ui.dialog('合并', '');
+								let styles = document.getSharedTextStyles()
+								let names = styles.map(x => {
+												return x.name;
+								});
+								let layers = document.selectedLayers
+								layers.forEach(x => {
+												let label = ui.label(x.name);
+												dialog.addAccessoryView(label);
+								})
 
-  if (responseCode === 1000) {
-      let selected = null;
-      let index = 0;
-      let selectedIndex = 0;
-      selection.forEach((x) => {
-        if (parseInt((checkboxs[index].stringValue()))) {
-          selected = x;
-          selectedIndex = index;
-        }
-        index++;
-      })
-      let index2 = 0;
-      let otherIds = [];
-      selection.forEach(x => {
-        if (index2 === selectedIndex) {
-          index2++;
-          return;
-        }
-        otherIds.push(x.symbolId);
-        try {
-          var copy = selected.duplicate();
-          copy.parent = x.parent;
-          copy.frame.x = x.frame.x;
-          copy.frame.y = x.frame.y;
-          copy.text = x.text,
-          x.parent = null;
-        }catch(e){
-            UI.alert('error', e.toString())
-        }
-        index2++;
-      })
-  }
+								let to = ui.popupButton(names)
+								dialog.addAccessoryView(to);
+								let responseCode = dialog.runModal();
+								if (responseCode === 1000) {
+												let fromValue = [];
+												layers.forEach(x => {
+																fromValue.push(x.sharedStyleId)
+												})
+												let toIndex = to.indexOfSelectedItem();
+												let toValue = styles[toIndex];
+												let document = dom.getSelectedDocument();
+
+												document.pages.forEach(x => {
+																changePage(x, fromValue, toValue);
+												})
+
+								}
+
+				}catch(e) {
+								UI.alert('error', e.toString())
+				}
 }
