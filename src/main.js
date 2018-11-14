e0ceea566d83bbbc1434fd6d25c0999b264e7db3
wangweipeng <weipeng.wang@gllue.com>
2018-11-14 18:08:12 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,54 +2,62 @@ const sketch = require('sketch')
 var ui = require('./ui')
 var UI = require('sketch/ui')
 var dom = require('sketch/dom')
-
+var layout = require('./layout')
 
 function changePage(one, from, to) {
-				if(from.indexOf(to.id) > -1) {
-								one.sharedStyleId = to.id;
-				}
-				one.affectedLayer && changePage(one.affectedLayer, from, to)
-				one.overrides && one.overrides.forEach(y => changePage(y, from, to))
-				one.layers && one.layers.forEach(y => changePage(y, from, to))
+	if (from.indexOf(to.id) > -1) {
+		one.sharedStyleId = to.id;
+	}
+	one.affectedLayer && changePage(one.affectedLayer, from, to)
+	one.overrides && one.overrides.forEach(y => changePage(y, from, to))
+	one.layers && one.layers.forEach(y => changePage(y, from, to))
 
 }
 
 
 
-export default function(context) {
-				try{
+export default function (context) {
+	try {
 
-								const document = sketch.fromNative(context.document)
-								let dialog = ui.dialog('合并', '');
-								let styles = document.getSharedTextStyles()
-								let names = styles.map(x => {
-												return x.name;
-								});
-								let layers = document.selectedLayers
-								layers.forEach(x => {
-												let label = ui.label(x.name);
-												dialog.addAccessoryView(label);
-								})
+		const document = sketch.fromNative(context.document)
+		let dialog = ui.dialog('合并', '');
+		let styles = document.getSharedTextStyles()
+		let names = styles.map(x => {
+			return x.name;
+		});
+		let checkboxs = [];
+		let scrollview = ui.scrollview();
+		scrollview.setHasVerticalScroller(true);
+		scrollview.setHasHorizontalScroller(true);
+		let scrollContent = new layout(0, 0, 300, 600);
+		names.forEach((x, y) => {
+			let checkbox = ui.checkbox(false, x, y * 20);
+			scrollContent.add(checkbox);
+			checkboxs.push(checkbox);
+		})
+		scrollview.setDocumentView(scrollContent.view()); dialog.addAccessoryView(scrollview);
+		let to = ui.popupButton(names)
 
-								let to = ui.popupButton(names)
-								dialog.addAccessoryView(to);
-								let responseCode = dialog.runModal();
-								if (responseCode === 1000) {
-												let fromValue = [];
-												layers.forEach(x => {
-																fromValue.push(x.sharedStyleId)
-												})
-												let toIndex = to.indexOfSelectedItem();
-												let toValue = styles[toIndex];
-												let document = dom.getSelectedDocument();
-
-												document.pages.forEach(x => {
-																changePage(x, fromValue, toValue);
-												})
-
-								}
-
-				}catch(e) {
-								UI.alert('error', e.toString())
+		dialog.addAccessoryView(to);
+		let responseCode = dialog.runModal();
+		if (responseCode === 1000) {
+			let ids = [];
+			checkboxs.forEach((x, y) => {
+				if (parseInt(x.stringValue())) {
+					ids.push(styles[y].sharedStyleId)
 				}
+			})
+			let toIndex = to.indexOfSelectedItem();
+			let toValue = styles[toIndex];
+			let document = dom.getSelectedDocument();
+
+			document.pages.forEach(x => {
+				changePage(x, ids, toValue);
+			})
+
+		}
+
+	} catch (e) {
+		UI.alert('error', e.toString())
+	}
 }
--- src/ui.js
+++ src/ui.js
@@ -1,79 +1,85 @@
 const ui = {};
 
-ui.dialog = function(title, info) {
-    let dialog = COSAlertWindow.alloc().init();
-    dialog.setMessageText(title);
-    if (info) {
-        dialog.setInformativeText(info);
-    }
-    dialog.addButtonWithTitle('OK');
-    dialog.addButtonWithTitle('Cancel');
-    return dialog;
+ui.dialog = function (title, info) {
+	let dialog = COSAlertWindow.alloc().init();
+	dialog.setMessageText(title);
+	if (info) {
+		dialog.setInformativeText(info);
+	}
+	dialog.setIcon(null);
+	dialog.addButtonWithTitle('OK');
+	dialog.addButtonWithTitle('Cancel');
+	return dialog;
 };
 
-ui.checkbox = function(status, title) {
-    let view = NSButton.alloc().initWithFrame(NSMakeRect(0, 0, 300, 24));
-    view.setButtonType(NSSwitchButton);
-    view.setTitle(title);
-    if (status == true) {
-        view.setState(NSOnState);
-    } else {
-        view.setState(NSOffState);
-    }
-    return view;
+ui.scrollview = function () {
+	let view = NSScrollView.alloc().initWithFrame(NSMakeRect(0, 0, 300, 600));
+	return view;
+}
+
+ui.checkbox = function (status, title, y) {
+	let view = NSButton.alloc().initWithFrame(NSMakeRect(0, y, 300, 24));
+	view.setButtonType(NSSwitchButton);
+	view.setTitle(title);
+	if (status == true) {
+		view.setState(NSOnState);
+	} else {
+		view.setState(NSOffState);
+	}
+	return view;
 };
 
-ui.label = function(text) {
-    let view = NSTextField.alloc().initWithFrame(NSMakeRect(0, 0, 300, 16));
-    view.setStringValue(text);
-    view.setBezeled(false);
-    view.setDrawsBackground(false);
-    view.setEditable(false);
-    view.setSelectable(false);
-    return view;
+ui.label = function (text) {
+	let view = NSTextField.alloc().initWithFrame(NSMakeRect(0, 0, 300, 16));
+	view.setStringValue(text);
+	view.setBezeled(false);
+	view.setDrawsBackground(false);
+	view.setEditable(false);
+	view.setSelectable(false);
+	return view;
 };
 
-ui.input = function(text) {
-    let view = NSTextField.alloc().initWithFrame(NSMakeRect(0, 0, 300, 24));
-    view.setStringValue(text);
-    return view;
+ui.input = function (text) {
+	let view = NSTextField.alloc().initWithFrame(NSMakeRect(0, 0, 300, 24));
+	view.setStringValue(text);
+	return view;
 };
 
-ui.numberInput = function(text) {
-    let view = NSTextField.alloc().initWithFrame(NSMakeRect(0, 0, 60, 24));
-    let formatter = NSNumberFormatter.alloc().init().autorelease();
-    formatter.setNumberStyle(NSNumberFormatterNoStyle);
-    view.setFormatter(formatter);
-    view.setStringValue(String(text));
-    return view;
+ui.numberInput = function (text) {
+	let view = NSTextField.alloc().initWithFrame(NSMakeRect(0, 0, 60, 24));
+	let formatter = NSNumberFormatter.alloc().init().autorelease();
+	formatter.setNumberStyle(NSNumberFormatterNoStyle);
+	view.setFormatter(formatter);
+	view.setStringValue(String(text));
+	return view;
 };
 
-ui.popupButton = function(items) {
-    let view = NSPopUpButton.alloc().initWithFrame(NSMakeRect(0, 0, 300, 24));
-    items.forEach(item => {
-        view.addItemWithTitle('');
-        view.lastItem().setTitle(item);
-    });
-    return view;
+ui.popupButton = function (items) {
+	let view = NSPopUpButton.alloc().initWithFrame(NSMakeRect(0, 0, 300, 24));
+	items.forEach(item => {
+		view.addItemWithTitle('');
+		view.lastItem().setTitle(item);
+	});
+	return view;
 };
 
-ui.stepper = function(min, max, defaultValue) {
-    let view = NSView.alloc().initWithFrame(NSMakeRect(0, 0, 300, 24));
-    let input = NSTextField.alloc().initWithFrame(NSMakeRect(0, 0, 60, 24));
-    input.setEditable(false);
-    input.setStringValue(String(defaultValue));
-    let stepper = NSStepper.alloc().initWithFrame(NSMakeRect(68, 0, 16, 24));
-    stepper.setMaxValue(max);
-    stepper.setMinValue(min);
-    stepper.setValueWraps(false);
-    stepper.setAutorepeat(true);
-    stepper.setIntegerValue(defaultValue);
-    view.addSubview(input);
-    view.addSubview(stepper);
-    stepper.setCOSJSTargetFunction(sender => {
-        input.setStringValue(String(sender.integerValue()));
-    });
-    return view;
+ui.stepper = function (min, max, defaultValue) {
+	let view = NSView.alloc().initWithFrame(NSMakeRect(0, 0, 300, 24));
+	let input = NSTextField.alloc().initWithFrame(NSMakeRect(0, 0, 60, 24));
+	input.setEditable(false);
+	input.setStringValue(String(defaultValue));
+	let stepper = NSStepper.alloc().initWithFrame(NSMakeRect(68, 0, 16, 24));
+	stepper.setMaxValue(max);
+	stepper.setMinValue(min);
+	stepper.setValueWraps(false);
+	stepper.setAutorepeat(true);
+	stepper.setIntegerValue(defaultValue);
+	view.addSubview(input);
+	view.addSubview(stepper);
+	stepper.setCOSJSTargetFunction(sender => {
+		input.setStringValue(String(sender.integerValue()));
+	});
+	return view;
 };
 
 module.exports = ui;
